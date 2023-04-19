89a17215f9cd31e5af617ee7089b312d8ff4abf6
EnricoBonato-web <bonny@devbook.local>
2023-04-19 19:50:42 +0200

    Updates

---
+++ precache-manifest.7773ed4ff34d8b43692ef725dc53d214.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "729d1daf04bb16f2c263",
+    "url": "/static/css/main.60f07906.chunk.css"
+  },
+  {
+    "revision": "729d1daf04bb16f2c263",
+    "url": "/static/js/main.729d1daf.chunk.js"
+  },
+  {
+    "revision": "d3406d199b610b0527b6",
+    "url": "/static/js/1.d3406d19.chunk.js"
+  },
+  {
+    "revision": "5a62db98dd68935c84a9",
+    "url": "/static/css/2.47c3ff21.chunk.css"
+  },
+  {
+    "revision": "5a62db98dd68935c84a9",
+    "url": "/static/js/2.5a62db98.chunk.js"
+  },
+  {
+    "revision": "91fd915e2311bea4e80e",
+    "url": "/static/js/3.91fd915e.chunk.js"
+  },
+  {
+    "revision": "29c9e5e0d1e9276351da",
+    "url": "/static/js/runtime~main.29c9e5e0.js"
+  },
+  {
+    "revision": "3c9241c2d284b85346b0c8e719568989",
+    "url": "/static/media/avatar.3c9241c2.svg"
+  },
+  {
+    "revision": "6e3ee94c3946b3f50f958de7118ad7f0",
+    "url": "/static/media/CvNew.6e3ee94c.pdf"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/static/media/pre.cf8c6935.svg"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/static/media/about.aee0f771.png"
+  },
+  {
+    "revision": "19c815003e6c32a54d6ad81af4fbb351",
+    "url": "/index.html"
+  }
+];
